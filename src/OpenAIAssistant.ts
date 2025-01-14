517622f9ae1ae07df5b017a92c1a6854cf431330
Toby Hinloopen <hinlomedia@gmail.com>
2025-01-14 08:31:03 +0100

    wip

--- src/OpenAIAssistant.ts
+++ src/OpenAIAssistant.ts
@@ -1,8 +1,5 @@
 import OpenAI from "openai";
 import { AITool } from "./AITool";
-import { realpathSync, writeFileSync } from "node:fs";
-import { formatDateAsISO } from "./formatDateAsISO";
-import chalk from "chalk";
 
 export class OpenAIAssistant {
   private openai = new OpenAI();
@@ -10,6 +7,10 @@ export class OpenAIAssistant {
   private tools: AITool[] = [];
 
   constructor(private model: OpenAI.Chat.ChatModel, private directory: string) {
+  }
+
+  public getMessages() {
+    return this.messages;
   }
 
   addTools(...tools: AITool[]) {
@@ -23,93 +24,80 @@ export class OpenAIAssistant {
     }
   }
 
+  async pushMessage(...messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[]) {
+    this.messages.push(...messages);
+  }
+
   /**
    * Send a next message and return the replies.
    * @param prompt
    */
   async send(prompt: string): Promise<OpenAIAssistant.Message> {
-    try {
-      this.messages.push({
-        role: "user",
-        content: [{ "type": "text", "text": prompt }]
-      });
+    return await this.sendMessage({
+      role: "user",
+      content: [{ type: "text", text: prompt }]
+    });
+  }
 
-      const body: OpenAI.Chat.Completions.ChatCompletionCreateParamsNonStreaming = {
-        model: this.model,
-        messages: this.messages,
-      };
+  async sendMessage(...messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[]) {
+    this.pushMessage(...messages);
+    return await this.run();
+  }
 
-      if (this.tools.length) {
-        body.tools = this.tools.map((aiTool) => ({
-          type: "function",
-          function: {
-            name: aiTool.name,
-            description: aiTool.description,
-            strict: true,
-            parameters: {
-              type: "object",
-              properties: aiTool.params,
-              additionalProperties: false,
-              required: Object.keys(aiTool.params),
-            }
+  async run(): Promise<OpenAIAssistant.Message> {
+    const body: OpenAI.Chat.Completions.ChatCompletionCreateParamsNonStreaming = {
+      model: this.model,
+      messages: this.messages,
+    };
+
+    if (this.tools.length) {
+      body.tools = this.tools.map((aiTool) => ({
+        type: "function",
+        function: {
+          name: aiTool.name,
+          description: aiTool.description,
+          strict: true,
+          parameters: {
+            type: "object",
+            properties: aiTool.params,
+            additionalProperties: false,
+            required: Object.keys(aiTool.params),
           }
-        }));
-      }
+        }
+      }));
+    }
 
-      let response: OpenAI.Chat.Completions.ChatCompletion;
+    let response: OpenAI.Chat.Completions.ChatCompletion;
 
-      while(true) {
-        response = await this.openai.chat.completions.create(body);
+    while(true) {
+      response = await this.openai.chat.completions.create(body);
 
-        const choice = response.choices[0];
-        this.messages.push(choice.message);
+      const choice = response.choices[0];
+      this.messages.push(choice.message);
 
-        if (choice.message.tool_calls?.length) {
-          for (const tool_call of choice.message.tool_calls) {
-            const tool = this.tools.find((tool) => tool.name === tool_call.function.name);
-            if (!tool)
-              throw new Error(`ChatGPT tried to call tool '${tool_call.function.name}' but it doesn't exist. Huh?`);
+      if (choice.message.tool_calls?.length) {
+        for (const tool_call of choice.message.tool_calls) {
+          const tool = this.tools.find((tool) => tool.name === tool_call.function.name);
+          if (!tool)
+            throw new Error(`ChatGPT tried to call tool '${tool_call.function.name}' but it doesn't exist. Huh?`);
 
-            const params = JSON.parse(tool_call.function.arguments);
-            const result = await tool?.run(params, this.directory);
-            this.messages.push({
-              role: "tool",
-              content: JSON.stringify(result),
-              tool_call_id: tool_call.id
-            });
-          }
-
-          body.messages = this.messages;
+          const params = JSON.parse(tool_call.function.arguments);
+          const result = await tool?.run(params, this.directory);
+          this.messages.push({
+            role: "tool",
+            content: JSON.stringify(result),
+            tool_call_id: tool_call.id
+          });
         }
 
-        if (choice.message.content) {
-          return choice.message as OpenAIAssistant.Message;
-        }
+        body.messages = this.messages;
       }
-    } finally {
-      const log = `logs/${formatDateAsISO()}.json`;
-      try {
-        writeFileSync(log, safeStringify(this.messages));
-        console.log(chalk.gray(`LOG: ${realpathSync(log)}`));
-      } catch (error) {
-        console.error(`Failed to write log:`);
-        console.error(error);
+
+      if (choice.message.content) {
+        return choice.message as OpenAIAssistant.Message;
       }
     }
   }
-}
-
-function safeStringify(obj: any): string {
-  const seen = new WeakSet();
-  return JSON.stringify(obj, (key, value) => {
-    if (typeof value === "object" && value !== null) {
-      if (seen.has(value)) {
-        return; // Skip circular reference
-      }
-      seen.add(value);
-    }
-    return value;
-  }, 2);
 }
 
 export namespace OpenAIAssistant {
--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,87 @@ import { OpenAIAssistant } from "./OpenAIAssistant";
 import { PromptTool } from "./tools/PromptTool";
 import { FileTools } from "./tools/FileTools";
 
-if (true) {
+import { readFileSync, realpathSync, writeFileSync } from "node:fs";
+import { formatDateAsISO } from "./formatDateAsISO";
+import chalk from "chalk";
+import { execSync } from "node:child_process";
+import { ChatCompletionMessageParam } from "openai/resources";
+
+async function runAssistant() {
   const prompt = process.argv.pop();
-  const dir = OpenAIAssistantManager.ensureWorkingDirectory(process.argv.pop());
+  const dirOrContinue = process.argv.pop();
+  const cont = dirOrContinue && dirOrContinue.includes("--continue");
+  const dir = OpenAIAssistantManager.ensureWorkingDirectory(cont ? process.argv.pop() : dirOrContinue);
   console.log("WORKING DIR", dir);
 
   const assistant = new OpenAIAssistant("gpt-4o", dir);
-  assistant.addTools(
-    new FileTools.LoggingReader(),
-    new FileTools.LoggingWriter(),
-    new FileTools.LoggingShellTool(),
-    new PromptTool(),
-  );
-  assistant.send(prompt!).then((x) => console.log(x.content), console.error);
+
+  if (cont) {
+    const logFile = execSync("ls -t logs/*.json | head -n 1", { encoding: "utf-8" }).trimEnd();
+    if (!logFile) {
+      throw new Error(`Can't continue - there is no log file?`);
+    }
+    const content = readFileSync(logFile, { encoding: "utf-8" });
+    const messages = JSON.parse(content) as ChatCompletionMessageParam[];
+    const messagesSlice = [messages[0]].concat(messages.slice(Math.max(messages.length - 4, 1)));
+
+    console.log(`Restored ${messagesSlice.length} of ${messages.length} messages from the previous conversation.`);
+    assistant.pushMessage(...messagesSlice);
+
+    console.log(chalk.grey(last(messagesSlice)?.content));
+  }
+
+  try {
+    assistant.addTools(
+      new FileTools.LoggingReader(),
+      new FileTools.LoggingWriter(),
+      new FileTools.LoggingShellTool(),
+      new PromptTool(),
+    );
+    const result = await assistant.send(prompt!);
+    console.log(result.content);
+
+    const summary = await assistant.sendMessage({
+      role: "system",
+      content: [{ type: "text", text: `
+        Summerize the conversation so a future AI agent or human can continue to
+        work on it. Include technical details!
+      `.replace(/\s+/g, " ") }]
+    });
+
+    console.log(chalk.gray(summary.content));
+  } finally {
+    const messages = assistant.getMessages();
+    const log = `logs/${formatDateAsISO()}.json`;
+    try {
+      writeFileSync(log, safeStringify(messages));
+      console.log(chalk.gray(`LOG: ${realpathSync(log)}`));
+    } catch (error) {
+      console.error(`Failed to write log:`);
+      console.error(error);
+    }
+  }
+
+  process.exit(0);
+
+  return "";
+}
+
+function safeStringify(obj: any): string {
+  const seen = new WeakSet();
+  return JSON.stringify(obj, (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return; // Skip circular reference
+      }
+      seen.add(value);
+    }
+    return value;
+  }, 2);
+}
+
+if (true) {
+  runAssistant().then(console.log, console.error);
 } else if (false)
   new OpenAIChatWorker().run(last(process.argv) as string);
 else if (false) {
