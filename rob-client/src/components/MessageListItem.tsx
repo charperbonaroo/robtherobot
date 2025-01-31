fbbf03197e85d31dbc496f000f358fca70cafd55
Toby Hinloopen <hinlomedia@gmail.com>
2025-01-31 17:25:07 +0100

    wip

--- rob-client/src/components/MessageListItem.tsx
+++ rob-client/src/components/MessageListItem.tsx
@@ -1,12 +1,12 @@
 import { RobWeb } from "rob-web";
-import styles from "./MessageListItem.module.css";
 import React from "react";
 import { classNames } from "../util/classNames";
 import { marked } from "marked";
+import styles from "./MessageListItem.module.css";
 
 export function MessageListItem(props: MessageListItem.Props) {
   const contentHtml = marked(props.message.content);
-  return <div className={classNames("card", "p-1", styles.root, styles[props.message.role])}>
+  return <div className={classNames(styles.root, "mb-2", "rounded-3", styles[props.message.role])}>
     <div className={styles.content} dangerouslySetInnerHTML={{ __html: contentHtml }} />
   </div>;
 }
