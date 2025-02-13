5630ff5a8e2b12b2fa7fd7ccfb8bfe0a23737033
Toby Hinloopen <hinlomedia@gmail.com>
2025-02-13 17:49:08 +0100

    autoscroll works

--- rob-gui/src/util/AutoscrollView.ts
+++ rob-gui/src/util/AutoscrollView.ts
@@ -1,34 +1,8 @@
-// import React, { useEffect, useRef } from "react";
-// import styles from "./AutoscrollView.module.css";
-
 import { css, html, LitElement } from "lit";
-
-// export function AutoscrollView(props: AutoscrollView.Props) {
-//   const ref = useRef<HTMLDivElement | null>(null);
-
-//   useEffect(() => {
-//     const container = ref.current;
-//     if (!container) return;
-//     container.scrollTop = container.scrollHeight;
-//   }, [props.children]);
-
-//   return <div ref={ref} className={styles.root}>{props.children}</div>;
-// }
-
-// export namespace AutoscrollView {
-//   export interface Props {
-//     children?: React.ReactNode;
-//   }
-// }
-
 
 export class AutoscrollView extends LitElement {
   static styles = css`
     :host {
-      display: contents;
-    }
-
-    .root {
       flex: 1 1 auto;
       overflow-y: auto;
       max-height: 100%;
@@ -36,7 +10,18 @@ export class AutoscrollView extends LitElement {
   `;
 
   render() {
-    return html`<div class=root><slot /></div>`;
+    return html`<div class=content><slot /></div>`;
+  }
+
+  firstUpdated() {
+    new ResizeObserver(() => this.scrollToBottom()).observe(
+      this.shadowRoot!.querySelector(".content")!);
+
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom() {
+    this.scrollTop = this.scrollHeight;
   }
 }
 
